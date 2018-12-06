693ac55a312d68896840cc23ff00579da1d5d715
zTogether <zhouucchao@163.com>
2018-12-06 17:18:31 +0800

    123

--- src/main/java/cn/xyzs/common/pojo/XyStarEvaList.java
+++ src/main/java/cn/xyzs/common/pojo/XyStarEvaList.java
@@ -11,12 +11,30 @@ public class XyStarEvaList {
     @Column(name = "EVA_NO")
     private String evaNo;
 
-    @Column(name = "LEVEL")
-    private String level;
+    @Column(name = "QUALITY")
+    private String quality;
 
     @Column(name = "EVALUATION")
     private String evaluation;
 
     @Column(name = "EVA_NAME")
     private String evaName;
+
+    @Column(name = "EVA_CODE")
+    private String evaCode;
+
+    @Column(name = "EVA_TYPE")
+    private String evaType;
+
+    @Column(name = "EVA_DATE")
+    private String evaDate;
+
+    @Column(name = "SERVICE")
+    private String service;
+
+    @Column(name = "DAYS")
+    private String days;
+
+    @Column(name = "HYGIENE")
+    private String hygiene;
 }
